3955fe98dff811dfd6ce49410c37c73af73cf916
yossefsabry <yossefsabry66@gmail.com>
2024-07-16 16:18:16 +0000

    adding some update for jwtcourse

--- jwtcourse/server/src/middleware/auth.mjs
+++ jwtcourse/server/src/middleware/auth.mjs
@@ -1,29 +1,25 @@
-
 import { verifyToken } from "../utlis/TokenGenerator.mjs";
 
-
 export const isAuth = async (req, res, done) => {
-
-  const { authorization } = req.headers;
-  if ( authorization == undefined ) {
-    return res.status(401).json({ msg: "undefined token"});
-  }
-  const splitAuthorization = authorization.split(' ');
-  // check if the token is provided
-  if (!splitAuthorization[0]?.startsWith(process.env.BARER_KEY)) {
-    return res.status(401).json({ msg: "not valid barer key"});
-  }
-  // get the token
-  const token = authorization.split(" ")[1];
-  try {
-    const decoded = verifyToken({ token }); // decoded
-    if (decoded === null) {
-      return res.status(401).json({ msg: "not valid token"});
-    }
-  }catch(error) {
-    return res.status(400).json({error:error});
-  }
-  req.jwt = decoded;
-  done();
-
+	const { authorization } = req.headers;
+	if (authorization == undefined) {
+		return res.status(401).json({ msg: "undefined token" });
+	}
+	const splitAuthorization = authorization.split(" ");
+	// check if the token is provided
+	if (!splitAuthorization[0]?.startsWith(process.env.BARER_KEY)) {
+		return res.status(401).json({ msg: "not valid barer key" });
+	}
+	// get the token
+	const token = authorization.split(" ")[1];
+	try {
+		const decoded = verifyToken({ token }); // decoded
+		if (decoded === null) {
+			return res.status(401).json({ msg: "not valid token" });
+		}
+	} catch (error) {
+		return res.status(400).json({ error: error });
+	}
+	req.jwt = decoded;
+	done();
 };
